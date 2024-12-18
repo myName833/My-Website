ed3c97fc523d096fb66dc1c595c6837cd708809c
Johnny Hsieh <johnnyhsieh0203@gmail.com>
2024-12-17 22:08:55 -0800

    change smth

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './pages/App';
-import MoreInfo from './pages/MoreInfo';
-import Project from './pages/Project';
-import GetInTouch from './pages/GetInTouch';
+import App from './pages/App'; // This is your home page component
+import MoreInfo from './pages/MoreInfo'; // Your 'MoreInfo' page component
+import Project from './pages/Project'; // Your 'Project' page component
+import GetInTouch from './pages/GetInTouch'; // Your 'GetInTouch' page component
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/My-Website"> {/* Add the basename */}
+    <BrowserRouter basename="/My-Website"> {/* Base path for GitHub Pages */}
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/moreinfo" element={<MoreInfo />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/getintouch" element={<GetInTouch />} />
+        {/* Define routes for different components */}
+        <Route path="/" element={<App />} /> {/* Home page */}
+        <Route path="/moreinfo" element={<MoreInfo />} /> {/* MoreInfo page */}
+        <Route path="/project" element={<Project />} /> {/* Project page */}
+        <Route path="/getintouch" element={<GetInTouch />} /> {/* GetInTouch page */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
