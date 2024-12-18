678fab4d1fc5095c93827affcb4d6f33de8220e1
Johnny Hsieh <johnnyhsieh0203@gmail.com>
2024-12-18 14:38:22 -0800

    change brouser router to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './pages/App'; // This is your home page component
-import MoreInfo from './pages/MoreInfo'; // Your 'MoreInfo' page component
-import Project from './pages/Project'; // Your 'Project' page component
-import GetInTouch from './pages/GetInTouch'; // Your 'GetInTouch' page component
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App from './pages/App';
+import MoreInfo from './pages/MoreInfo';
+import Project from './pages/Project';
+import GetInTouch from './pages/GetInTouch';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/My-Website/"> {/* Base path for GitHub Pages */}
+    <HashRouter>
       <Routes>
-        {/* Define routes for different components */}
-        <Route path="*" element={<App />} /> {/* Home page */}
-        <Route path="/moreinfo" element={<MoreInfo />} /> {/* MoreInfo page */}
-        <Route path="/project" element={<Project />} /> {/* Project page */}
-        <Route path="/getintouch" element={<GetInTouch />} /> {/* GetInTouch page */}
+        <Route path="/" element={<App />} /> {/* Home page */}
+        <Route path="moreinfo" element={<MoreInfo />} />
+        <Route path="project" element={<Project />} />
+        <Route path="getintouch" element={<GetInTouch />} />
+        <Route path="*" element={<App />} /> {/* Catch-all route */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
